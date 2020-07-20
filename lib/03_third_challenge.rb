744a52ce8ca5b47b6873ebaf5421b2bf6530e752
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-20 18:38:04 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -15,13 +15,15 @@ def third_challenge
         status: "alive"
         },
       :hero_friends => [
-         {name: "Benvolio",
-        age: "17",
-        attitude: "worried"},
+         {
+           :name: "Benvolio",
+        :age: "17",
+        :attitude: "worried"},
         
-        {name: "Mercutio",
-        age: "18",
-        attitude: "hot-headed"}
+        {
+          :name: "Mercutio",
+        :age: "18",
+        :attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
@@ -38,9 +40,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => [
-       
-        ]
+      :heroine_friends => []
    }
   }
 
