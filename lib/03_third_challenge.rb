3834bf18af1826984cc4701206acfc2e786f9422
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-20 18:30:01 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Montague"
+        name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
@@ -18,16 +18,16 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
-        name: "Juliet"
-        age: "15"
+        name: "Juliet",
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
